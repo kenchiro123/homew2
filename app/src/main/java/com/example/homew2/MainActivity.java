ae0099770d077cae4fb8f829f7f55018133d11bc
kenchiro123 <chanon.ken@gmail.com>
2019-08-21 18:00:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/homew2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.homew2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast t = Toast.makeText(this,"Welcome to My ToDo application!",Toast.LENGTH_SHORT);
+        t.show();
+
+    }
+}
